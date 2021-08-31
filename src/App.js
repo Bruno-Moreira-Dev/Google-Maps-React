acdd335aa6d59eb1b4a6fd692b3d2371f9ec2222
Bruno Pavan <bruno@followize.com.br>
2021-08-31 17:55:31 -0300

    feat: integracao com google-maps
    Resolve:[master]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { compose, withStateHandlers } from 'recompose';
+import {  useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Map = compose(
+  withStateHandlers(() => ({
+    isMarkerShown: false,
+    markerPosition: null
+  }),
+    {
+      onMapClick: () => (e) => ({
+        markerPosition: e.latLng,
+        isMarkerShown: true,
+      })
+    }),
+    useJsApiLoader
+)
+  (props =>
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -23.184660, lng: -47.277512 }}
+      onClick={props.onMapClick}
+    >
+      {props.isMarkerShown && <Marker position={props.markerPosition} />}
+    </GoogleMap>
+  )
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZN71Y-8KpjRbqjPkn9OnNqTjbllLU4Dk"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        position={{ lat: -23.184660, lng: -47.277512 }}
+      />
+    )
+  }
 }
-
-export default App;
